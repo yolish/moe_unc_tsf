af4fd12dbfdf4bde53390bfc92ef4b373230c5c9
Yoli Shavit <yolisha@gmail.com>
2025-08-04 22:44:41 +0300

    working bash script to run experiments across multiple configurations

--- scripts/example.sh
+++ scripts/example.sh
@@ -1,4 +1,93 @@
-python run.py --task_name long_term_forecast --is_training 1 --model_id test --model PatchTST --data ETTh1 
-python run.py --task_name long_term_forecast --is_training 1 --model_id test --model PatchTST --data ETTh1 --num_experts 3
-python run.py --task_name long_term_forecast --is_training 1 --model_id test --model PatchTST --data ETTh1 --num_experts 3 --prob_expert 
-python run.py --task_name long_term_forecast --is_training 1 --model_id test --model PatchTST --data ETTh1 --num_experts 3 --prob_expert --unc_gating
+models=("PatchTST")
+root_paths=("./data/long_term_forecast/ETT/")
+data_paths=("ETTh1.csv") 
+datasets=("ETTh1")
+pred_lengths=(96 192)
+num_experts=(1 3)
+configurations=(0 1 2)
+seeds=(2021)
+model_id="test"
+features="M"
+seq_len=96
+label_len=48
+
+
+length=${#root_paths[@]}
+for seed in "${seeds[@]}"
+do
+  for model_name in "${models[@]}"
+  do
+    for ((i=0; i<$length; i++))
+    do
+      for pred_len in "${pred_lengths[@]}"
+      do
+        root_path=${root_paths[$i]}
+        data_path=${data_paths[$i]}
+        dataset=${datasets[$i]}
+        for ne in ${num_experts[@]}
+            do
+            for config in "${configurations[@]}"
+              do
+                    
+                    if [ $config -eq 1 ]; then
+                        echo "python -u run.py --task_name long_term_forecast --root_path $root_path --data_path $data_path --model $model_name --data $dataset --pred_len $pred_len --num_experts $ne"
+                        python -u run.py \
+                        --task_name long_term_forecast \
+                        --is_training 1 \
+                        --root_path $root_path \
+                        --data_path $data_path \
+                        --model_id $model_id \
+                        --model $model_name \
+                        --data $dataset \
+                        --features $features \
+                        --seq_len $seq_len \
+                        --label_len $label_len \
+                        --pred_len $pred_len \
+                        --seed $seed \
+                        --num_experts $ne 
+                    fi
+                    if [ $config -eq 2 ]; then
+                        echo "python -u run.py --task_name long_term_forecast --root_path $root_path --data_path $data_path --model $model_name --data $dataset --pred_len $pred_len --num_experts $ne --prob_expert"
+                        python -u run.py \
+                        --task_name long_term_forecast \
+                        --is_training 1 \
+                        --root_path $root_path \
+                        --data_path $data_path \
+                        --model_id $model_id \
+                        --model $model_name \
+                        --data $dataset \
+                        --features $features \
+                        --seq_len $seq_len \
+                        --label_len $label_len \
+                        --pred_len $pred_len \
+                        --seed $seed \
+                        --num_experts $ne \
+                        --prob_expert
+                    fi
+                    if [ $config -eq 2 ]; then
+                        echo "python -u run.py --task_name long_term_forecast --root_path $root_path --data_path $data_path --model $model_name --data $dataset --pred_len $pred_len --num_experts $ne --prob_expert --unc_gating"
+                        python -u run.py \
+                        --task_name long_term_forecast \
+                        --is_training 1 \
+                        --root_path $root_path \
+                        --data_path $data_path \
+                        --model_id $model_id \
+                        --model $model_name \
+                        --data $dataset \
+                        --features $features \
+                        --seq_len $seq_len \
+                        --label_len $label_len \
+                        --pred_len $pred_len \
+                        --seed $seed \
+                        --num_experts $ne \
+                        --prob_expert \
+                        --unc_gating
+                    fi
+                    
+                done
+            done    
+        done
+    done
+done
+done
+

0b63fe8d5b4439b3ed92a1ab4982059fdbbb446f
Yoli Shavit <yolisha@gmail.com>
2025-08-05 23:20:07 +0300

    Changed gating architecture and updated expert weights to be per time step per variable

--- models/MoE.py
+++ models/MoE.py
@@ -1,85 +1,30 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from layers.Autoformer_EncDec import series_decomp
+from models.PatchTST import Model as GatingModel
+import copy
 
-
-class Gating(nn.Module): # based on DLinear model 
+class Gating(nn.Module): 
     def __init__(self, configs, individual=False):
         """
         individual: Bool, whether shared model among different variates.
         """
         super(Gating, self).__init__()
-        self.task_name = configs.task_name
-        self.seq_len = configs.seq_len
-        # Series decomposition block from Autoformer
-        self.decompsition = series_decomp(configs.moving_avg)
-        self.individual = individual
-        self.channels = configs.enc_in
-        self.pred_len = configs.pred_len
-        self.num_experts = configs.num_experts
-
-        if self.individual:
-            self.Linear_Seasonal = nn.ModuleList()
-            self.Linear_Trend = nn.ModuleList()
-
-            for i in range(self.channels):
-                self.Linear_Seasonal.append(
-                    nn.Linear(self.seq_len, self.pred_len))
-                self.Linear_Trend.append(
-                    nn.Linear(self.seq_len, self.pred_len))
-
-                self.Linear_Seasonal[i].weight = nn.Parameter(
-                    (1 / self.seq_len) * torch.ones([self.pred_len, self.seq_len]))
-                self.Linear_Trend[i].weight = nn.Parameter(
-                    (1 / self.seq_len) * torch.ones([self.pred_len, self.seq_len]))
-        else:
-            self.Linear_Seasonal = nn.Linear(self.seq_len, self.pred_len)
-            self.Linear_Trend = nn.Linear(self.seq_len, self.pred_len)
-
-            self.Linear_Seasonal.weight = nn.Parameter(
-                (1 / self.seq_len) * torch.ones([self.pred_len, self.seq_len]))
-            self.Linear_Trend.weight = nn.Parameter(
-                (1 / self.seq_len) * torch.ones([self.pred_len, self.seq_len]))
-
-        
-        self.projection = nn.Linear(
-            configs.enc_in * configs.seq_len, configs.num_experts*configs.pred_len)
-
-    def encoder(self, x):
-        seasonal_init, trend_init = self.decompsition(x)
-        seasonal_init, trend_init = seasonal_init.permute(
-            0, 2, 1), trend_init.permute(0, 2, 1)
-        if self.individual:
-            seasonal_output = torch.zeros([seasonal_init.size(0), seasonal_init.size(1), self.pred_len],
-                                          dtype=seasonal_init.dtype).to(seasonal_init.device)
-            trend_output = torch.zeros([trend_init.size(0), trend_init.size(1), self.pred_len],
-                                       dtype=trend_init.dtype).to(trend_init.device)
-            for i in range(self.channels):
-                seasonal_output[:, i, :] = self.Linear_Seasonal[i](
-                    seasonal_init[:, i, :])
-                trend_output[:, i, :] = self.Linear_Trend[i](
-                    trend_init[:, i, :])
-        else:
-            seasonal_output = self.Linear_Seasonal(seasonal_init)
-            trend_output = self.Linear_Trend(trend_init)
-        x = seasonal_output + trend_output
-        return x.permute(0, 2, 1)
+        self.projection = nn.Linear(1, configs.num_experts)
+        configs = copy.deepcopy(configs)
+        configs.prob_expert = 0
+        self.gating_arc = GatingModel(configs)
 
     def gating(self, x_enc):
-        # Encoder
-        enc_out = self.encoder(x_enc)
-        # Output
-        # (batch_size, seq_length * d_model)
-        output = enc_out.reshape(enc_out.shape[0], -1)
-        output = self.projection(output)
-        return output
+        enc_out = self.gating_arc.forecast(x_enc, None, None, None) # batch_size, seq_len, variate_dim
+        enc_out = enc_out.unsqueeze(-1)
+        weights = self.projection(enc_out)
+        return weights # batch_size, seq_len, variate_dim, num_experts
 
     def forward(self, x_enc):
-        dec_out = self.gating(x_enc)
-        # reshape and pass through softmax to give weights per expert 
-        weights = dec_out.reshape(dec_out.shape[0], self.num_experts, self.pred_len)
-        weights = F.softmax(weights, dim=1)
+        weights = self.gating(x_enc)
+        weights = F.softmax(weights, dim=-1)
+        weights = weights.permute(0, 3, 1, 2)
         return weights
 
 
@@ -115,18 +60,12 @@ class Model(nn.Module):
             if len(expert_unc) > 0:
                 expert_unc = torch.stack(expert_unc, dim=1) # [batch_size, num_experts, pred_len, num_features]
             if self.unc_gating:
-                # Average across features to get per-expert uncertainty
-                expert_unc_avg = torch.mean(expert_unc, dim=-1)  # [batch_size, num_experts, pred_len]
                 # Inverse variance weighting: higher confidence (lower uncertainty) gets higher weight
-                inv_var = 1.0 / (expert_unc_avg + 1e-8)  # [batch_size, num_experts, pred_len]
-                sum_inv_var = torch.sum(inv_var, dim=1, keepdim=True)  # [batch_size, 1, pred_len]
-                weights = inv_var / sum_inv_var  #  [batch_size, num_experts, pred_len]
-                #weights = weights_temp.permute(1, 0, 2)  # [batch_size, num_experts, pred_len]
-            else:
-                weights = self.gating(x_enc) # [batch_size, num_experts, pred_len]     
-            # repet weights for each feature dimension 
-            num_featuers = expert_out.shape[-1]
-            weights = weights.unsqueeze(-1).repeat(1, 1, 1, num_featuers)   
+                inv_var = 1.0 / (expert_unc+ 1e-8)  # [batch_size, num_experts, pred_len, num_features]
+                sum_inv_var = torch.sum(inv_var, dim=1, keepdim=True)  # [batch_size, 1, pred_len, num_features]
+                weights = inv_var / sum_inv_var  #  [batch_size, num_experts, pred_len, num_featuers]            else:
+            else:    
+                weights = self.gating(x_enc) # [batch_size, num_experts, pred_len, num_featuers]   
             return expert_out, expert_unc, weights 
         else:
             raise NotImplementedError("{} not supported with MoE".format(self.task_name))

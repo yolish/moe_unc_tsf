113722b6b9013972d64a61467c90d012146108e9
Yoli Shavit <yolisha@gmail.com>
2025-08-17 23:41:47 +0300

    script for reproducing the main results

---
+++ run_all.sh
@@ -0,0 +1,8 @@
+bash scripts/run_tsf_ett.sh 0
+rm -r checkpoints
+bash scripts/run_tsf_ili.sh 0
+rm -r checkpoints
+bash scripts/run_tsf_large.sh 0
+rm -r checkpoints
+bash scripts/run_tsf_xl.sh 0
+rm -r checkpoints

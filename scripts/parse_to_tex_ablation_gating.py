16ff4b40e9a6f9990de2f0e33819ae5db4f7247b
Yoli Shavit <yolisha@gmail.com>
2025-09-03 10:05:33 +0300

    ablation scripts and tex table prep scripts

--- scripts/parse_to_tex_ablation_gating.py
+++ scripts/parse_to_tex_ablation_gating.py
@@ -11,67 +11,38 @@ def to_value(a):
         return -1
 
 df = pd.read_csv("moe_results.csv")
-domains_to_names = {"national-illness":"ILI",
-                    "ETTh1":"ETTh1","ETTh2":"ETTh2","ETTm1":"ETTm1","ETTm2":"ETTm2"}
-experts = ["DLinear", "iTransformer", "PatchTST"]
+domains_to_names = {"ETTh1":"ETTh1","ETTh2":"ETTh2","ETTm1":"ETTm1","ETTm2":"ETTm2"}
+experts = ["iTransformer", "PatchTST"]
 
 
 
 print("")
 domains = np.sort(list(domains_to_names.keys()))
-for domain in domains:
-    domain_name = domains_to_names[domain]
-    if domain == "national-illness":
-        pred_lens = [24, 36, 48, 60]
-    else:
-        pred_lens = [96, 192, 336, 720]
-    for i, pl in enumerate(pred_lens):
+pl = 96
+for e in experts:
+    
+    for domain in domains:
+        domain_name = domains_to_names[domain]
         row = []
-        if i == 0:
-            row.append("&\\multirow{" + "4}*" + "{\\rotatebox{"+"90}{"+"{}".format(domain_name)+"}}")
-        else:
-            row.append("&\\multicolumn{"+"1}{c|}{}")
-        row.append(pl)
-        for e in experts:
-            domain_df = df[(df["dataset"] == domain) & (df["expert"] == e) & (df["pl"] == pl)]
+
+        row.append(e)
+        row.append(domain_name)
+
+        domain_df = df[(df["dataset"] == domain) & (df["expert"] == e) & (df["pl"] == pl)]
             
-            prob_moe_stan = domain_df[(domain_df["num_experts"] == 3) & \
+        prob_moe_stan = domain_df[(domain_df["num_experts"] == 3) & \
                                 (domain_df["prob_moe"] == 1) & (domain_df["unc_gating"] == 0)]
-            if prob_moe_stan.empty:
-               prob_stan_moe_mse = -1 
-            else:
-                prob_stan_moe_mse = to_value(prob_moe_stan["mse"].values)
+        
+        row.append(to_value(prob_moe_stan["mae"].values))
+        row.append(to_value(prob_moe_stan["mse"].values))
+        prob_moe = domain_df[(domain_df["num_experts"] == 3) & \
+                            (domain_df["prob_moe"] == 1) & (domain_df["unc_gating"] == 1)]
+        row.append(to_value(prob_moe["mae"].values))
+        row.append(to_value(prob_moe["mse"].values))
             
-            #det_moe_mae = to_value(det_moe["mae"].values)
-            prob_moe = domain_df[(domain_df["num_experts"] == 3) & \
-                                (domain_df["prob_moe"] == 1) & (domain_df["unc_gating"] == 1)]
-            if prob_moe.empty:
-                prob_moe_mse = -1 
-            else:
-                prob_moe_mse = to_value(prob_moe["mse"].values)
-            
-            if prob_stan_moe_mse == -1 or prob_moe_mse == -1:
-                if prob_stan_moe_mse == -1:
-                    row.append("M")
-                else:
-                    row.append("{:.3f}".format(prob_stan_moe_mse))
-                if prob_moe_mse == -1:
-                    row.append("M")
-                else:
-                    row.append("{:.3f}".format(prob_moe_mse))
-            else:
-                if prob_stan_moe_mse < prob_moe_mse:
-                    row.append("\\textbf{" + "{:.3f}".format(prob_stan_moe_mse) + "}")
-                    row.append("{:.3f}".format(prob_moe_mse))
-                elif prob_stan_moe_mse == prob_moe_mse:
-                    row.append("\\textbf{" + "{:.3f}".format(prob_stan_moe_mse) + "}")
-                    row.append("\\textbf{" + "{:.3f}".format(prob_moe_mse) + "}")
-                else:
-                    row.append("{:.3f}".format(prob_stan_moe_mse))
-                    row.append("\\textbf{" + "{:.3f}".format(prob_moe_mse) + "}")
-        print("{} & {} & {} & {} & {} & {} & {} & {} \\\\".format(*row))
+           
+        print("{} & {} & {:.3f} & {:.3f} & {:.3f} & {:.3f} \\\\".format(*row))
         #print("{} & {} & {} & {} & {} & {} \\\\".format(*row))
-    print("\\cline{2-9}")
     #print("\\cline{2-7}")
             
 
